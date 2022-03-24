2daf0f6c8e0b189efe7946cf5e1e861bdff1bb4b
Stanislav Sava <sava.stanislav@gmail.com>
2022-03-24 10:12:08 +0200

    update receiver validation

---
+++ src/validationSchema/receiver.ts
@@ -0,0 +1,37 @@
+import { addressIsValid } from '@elrondnetwork/dapp-core';
+import { string } from 'yup';
+import { TxTypeEnum, ExtendedValuesType } from 'types';
+
+const required = string().required('Required');
+
+const validAddress = string().test(
+  'addressIsValid',
+  'Invalid address',
+  (value) => Boolean(value && addressIsValid(value))
+);
+
+const sameAddress = string().test(
+  'sameAddress',
+  'Same as owner address',
+  function sameAddressCheck(value) {
+    const { ignoreTokenBalance, txType, readonly, address } = this
+      .parent as ExtendedValuesType;
+    const isNftTransaction = ![TxTypeEnum.EGLD, TxTypeEnum.ESDT].includes(
+      txType
+    );
+    const signContext = ignoreTokenBalance;
+    if (isNftTransaction && !signContext && !readonly) {
+      return address !== value;
+    }
+    return true;
+  }
+);
+
+const validations = [required, validAddress, sameAddress];
+
+export const receiver = validations.reduce(
+  (previousValue, currentValue) => previousValue.concat(currentValue),
+  string()
+);
+
+export default receiver;
