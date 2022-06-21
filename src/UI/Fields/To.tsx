1b98772e437a9476a284dbfee9f04646c76a78ec
Tudor Morar <tudor.morar@elrond.com>
2022-06-21 18:30:36 +0300

    [*] Upgrade Typeahead

--- src/UI/Fields/To.tsx
+++ src/UI/Fields/To.tsx
@@ -6,26 +6,35 @@ import classnames from 'classnames';
 import { Form } from 'react-bootstrap';
 import { Typeahead, Menu, MenuItem, Hint } from 'react-bootstrap-typeahead';
 import { MenuProps } from 'react-bootstrap-typeahead/types/components/Menu';
+import {
+  FilterByCallback,
+  SelectHint,
+  TypeaheadInputProps,
+  TypeaheadManagerChildProps,
+  Option
+} from 'react-bootstrap-typeahead/types/types';
+import { ReceiverContextPropsType } from 'contexts/ReceiverContext';
 import { useSendFormContext } from 'contexts/SendFormProviderContext';
 import { useUICustomizationContext } from 'contexts/UICustomization';
 
-function filterBy(option: any, props: any) {
+const filterBy: FilterByCallback = (option, props) => {
   if (props.text.length > 2) {
     return option.toLowerCase().indexOf(props.text.toLowerCase()) !== -1;
   }
   return false;
-}
-
-// const shouldSelect = (canSelect: boolean, e: any) => {
-//   return e.key === 'Enter' || e.keyCode === 13 || canSelect;
-// };
+};
 
 const renderMenu = (results: string[], menuProps: MenuProps) => {
   if (!results.length) {
     return null;
   }
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const { renderMenuItemChildren, referenceElementRef, ...rest } =
+    menuProps as any;
+
   return (
-    <Menu {...menuProps}>
+    <Menu {...rest}>
       {results.map((result, index) => {
         return index < 5 ? (
           <MenuItem option={result} position={index} key={index}>
@@ -37,18 +46,41 @@ const renderMenu = (results: string[], menuProps: MenuProps) => {
   );
 };
 
-const renderInput = ({ inputRef, referenceElementRef, ...inputProps }: any) => (
-  <Hint>
-    <Form.Control
-      {...inputProps}
-      data-testid='receiver'
-      ref={(node: any) => {
-        inputRef(node);
-        referenceElementRef(node);
-      }}
-    />
-  </Hint>
-);
+const changeAndBlurInput = (props: {
+  value?: string;
+  onBlurReceiver: ReceiverContextPropsType['onBlurReceiver'];
+  onChangeReceiver: ReceiverContextPropsType['onChangeReceiver'];
+}) => {
+  const noSpaces = props.value ? props.value.trim() : '';
+  props.onChangeReceiver(noSpaces);
+  setTimeout(() => {
+    props.onBlurReceiver(new Event('blur'));
+  });
+};
+
+const renderInput =
+  (
+    onBlurReceiver: ReceiverContextPropsType['onBlurReceiver'],
+    onChangeReceiver: ReceiverContextPropsType['onChangeReceiver']
+  ) =>
+  (inputProps: TypeaheadInputProps, props: TypeaheadManagerChildProps) => {
+    return (
+      <Hint>
+        <Form.Control
+          {...(props.getInputProps() as any)}
+          {...inputProps}
+          onBlur={() => {
+            changeAndBlurInput({
+              value: inputProps.value?.toString(),
+              onBlurReceiver,
+              onChangeReceiver
+            });
+          }}
+          data-testid='receiver'
+        />
+      </Hint>
+    );
+  };
 
 export const To = () => {
   const {
@@ -72,26 +104,27 @@ export const To = () => {
 
   const ref = React.useRef(null);
 
-  const typeaheadInputProps = {
-    id: 'receiver',
-    name: 'receiver',
-    autoCapitalize: 'none',
-    className: classnames('', {
-      scam: Boolean(scamError),
-      'is-invalid': isReceiverInvalid
-    })
+  const onInputChange = (value: string) => {
+    changeAndBlurInput({ value, onChangeReceiver, onBlurReceiver });
   };
 
-  const onInputChange = (input: string) => {
-    const noSpaces = input ? input.trim() : input;
-    onChangeReceiver(noSpaces);
+  const onChange = (selected: Option[]) => {
+    const selection = selected.pop()?.toString();
+    if (selection) {
+      onInputChange(selection);
+    }
   };
 
-  function triggerRerenderOnceOnHook() {
+  const triggerRerenderOnceOnHook = () => {
     if (addressIsValid(receiver) && !key) {
       setKey(receiver);
     }
-  }
+  };
+
+  const selectHint = (
+    shouldSelect: SelectHint,
+    event: React.KeyboardEvent<HTMLInputElement>
+  ) => event.key === 'Enter' || shouldSelect;
 
   useEffect(triggerRerenderOnceOnHook, [receiver]);
 
@@ -106,19 +139,16 @@ export const To = () => {
         <Typeahead
           ref={ref}
           id='receiverWrapper'
-          inputProps={typeaheadInputProps}
+          selectHint={selectHint as any}
           defaultInputValue={receiver}
           options={knownAddresses}
           ignoreDiacritics
           emptyLabel={false}
           caseSensitive={false}
           filterBy={filterBy}
+          onChange={onChange}
           onInputChange={onInputChange}
-          onBlur={(e) => {
-            onBlurReceiver(e as any);
-          }}
-          renderMenuItemChildren={(option) => <div>{option.toString()}</div>}
-          renderInput={renderInput}
+          renderInput={renderInput(onBlurReceiver, onChangeReceiver)}
           renderMenu={renderMenu as any}
         />
       </div>
