3cf758487f31ace2ca5a56fb77cbdebb4ab4e59e
Grigore Budac <contact@grigorebudac.com>
2022-06-24 12:45:15 +0200

    remove the nodejs shim from esbuild

--- executeBuildCommand.js
+++ executeBuildCommand.js
@@ -1,24 +1,10 @@
 const svgrPlugin = require('esbuild-plugin-svgr');
 const esbuild = require('esbuild');
 const glob = require('glob');
-const plugin = require('node-stdlib-browser/helpers/esbuild/plugin');
-const stdLibBrowser = require('node-stdlib-browser');
 const { nodeExternalsPlugin } = require('esbuild-node-externals');
 const { sassPlugin, postcssModules } = require('esbuild-sass-plugin');
-const path = require('path');
 
 const basedir = 'src';
-
-function createSassImporter({ basedir = process.cwd() }) {
-  const opts = { basedir, extensions: ['.scss', '.sass', '.css'] };
-
-  return function importer(url, prev) {
-    if (url.startsWith('~')) {
-      url = url.slice(1);
-    }
-    return { file: resolve.sync(url, opts) };
-  };
-}
 
 module.exports = function executeBuildCommand(customOptions = {}) {
   glob(
@@ -42,22 +28,10 @@ module.exports = function executeBuildCommand(customOptions = {}) {
           target: ['es2015'],
           tsconfig: './tsconfig.json',
           platform: 'node',
-          inject: [require.resolve('node-stdlib-browser/helpers/esbuild/shim')],
-          define: {
-            global: 'global',
-            process: 'process',
-            Buffer: 'Buffer',
-            'process.env.NODE_ENV': `"production"`
-          },
           plugins: [
             sassPlugin({
-              loadPaths: [
-                `./${basedir}`,
-                'node_modules'
-                // path.resolve(__dirname, 'node_modules/bootstrap/scss')
-              ],
+              loadPaths: [`./${basedir}`, 'node_modules'],
               basedir,
-              // quietDeps: true,
               cssImports: true,
               transform: postcssModules({
                 basedir,
@@ -65,7 +39,6 @@ module.exports = function executeBuildCommand(customOptions = {}) {
               })
             }),
             svgrPlugin(),
-            plugin(stdLibBrowser),
             nodeExternalsPlugin()
           ],
           ...customOptions
