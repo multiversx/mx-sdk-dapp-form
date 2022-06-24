8666d9e424c6dec7359fcb516cf61358fe66819e
Grigore Budac <contact@grigorebudac.com>
2022-06-24 15:42:47 +0200

    bundle all the ts/tsx files for more atomic imports

--- executeBuildCommand.js
+++ executeBuildCommand.js
@@ -7,43 +7,40 @@ const { sassPlugin, postcssModules } = require('esbuild-sass-plugin');
 const basedir = 'src';
 
 module.exports = function executeBuildCommand(customOptions = {}) {
-  glob(
-    '{./src/UI/index.tsx,./src/index.tsx,./src/**/*.scss}',
-    function (err, files) {
-      if (err) {
-        console.log('error reading files', err);
-      }
-
-      esbuild
-        .build({
-          entryPoints: files,
-          outdir: 'dist',
-          treeShaking: true,
-          minify: true,
-          bundle: true,
-          sourcemap: true,
-          splitting: true,
-          chunkNames: '__chunks__/[name]-[hash]',
-          format: 'esm',
-          target: ['es2015'],
-          tsconfig: './tsconfig.json',
-          platform: 'node',
-          plugins: [
-            sassPlugin({
-              loadPaths: [`./${basedir}`, 'node_modules'],
-              basedir,
-              cssImports: true,
-              transform: postcssModules({
-                basedir,
-                localsConvention: 'dashes'
-              })
-            }),
-            svgrPlugin(),
-            nodeExternalsPlugin()
-          ],
-          ...customOptions
-        })
-        .catch(() => process.exit(1));
+  glob('{./src/**/*.tsx,./src/**/*.ts,./src/**/*.scss}', function (err, files) {
+    if (err) {
+      console.log('error reading files', err);
     }
-  );
+
+    esbuild
+      .build({
+        entryPoints: files,
+        outdir: 'dist',
+        treeShaking: true,
+        minify: true,
+        bundle: true,
+        sourcemap: true,
+        splitting: true,
+        chunkNames: '__chunks__/[name]-[hash]',
+        format: 'esm',
+        target: ['es2015'],
+        tsconfig: './tsconfig.json',
+        platform: 'node',
+        plugins: [
+          sassPlugin({
+            loadPaths: [`./${basedir}`, 'node_modules'],
+            basedir,
+            cssImports: true,
+            transform: postcssModules({
+              basedir,
+              localsConvention: 'dashes'
+            })
+          }),
+          svgrPlugin(),
+          nodeExternalsPlugin()
+        ],
+        ...customOptions
+      })
+      .catch(() => process.exit(1));
+  });
 };
