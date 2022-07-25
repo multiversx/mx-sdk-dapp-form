107eadd0fddcb325a02a5ebadae5fc5afc8d0c29
Stanislav Sava <sava.stanislav@gmail.com>
2022-07-25 11:26:30 +0300

    fix initialValues initialized with 0 not setting correctly

--- src/SendFormContainer.tsx
+++ src/SendFormContainer.tsx
@@ -84,18 +84,18 @@ export function SendFormContainer(props: SendFormContainerPropsType) {
 
   const formikInitialValues = {
     tokenId,
-    receiver: initialValues?.receiver || '',
-    gasPrice: initialValues?.gasPrice || denominatedConfigGasPrice,
-    data: initialValues?.data || '',
-    amount: initialValues?.amount || '',
-    gasLimit: initialValues?.gasLimit || String(gasLimit),
+    receiver: initialValues?.receiver ?? '',
+    gasPrice: initialValues?.gasPrice ?? denominatedConfigGasPrice,
+    data: initialValues?.data ?? '',
+    amount: initialValues?.amount ?? '',
+    gasLimit: initialValues?.gasLimit ?? String(gasLimit),
     txType:
-      initialValues?.txType ||
+      initialValues?.txType ??
       getTxType({ nft: tokensInfo?.initialNft, tokenId }),
-    address: initialValues?.address || address,
+    address: initialValues?.address ?? address,
     nft: tokensInfo?.initialNft,
-    balance: initialValues?.balance || balance,
-    chainId: initialValues?.chainId || networkConfig.chainId,
+    balance: initialValues?.balance ?? balance,
+    chainId: initialValues?.chainId ?? networkConfig.chainId,
     tokens: tokensInfo?.initialTokens
   };
 
