ed9212e5f29f3ba402b69a1ed2a1d181f76acd04
Stanislav Sava <sava.stanislav@gmail.com>
2022-07-29 12:53:12 +0300

    clean up moduleDirectories

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   verbose: true,
   testEnvironment: 'jsdom',
-  moduleDirectories: ['node_modules', 'src', 'main'],
+  moduleDirectories: ['node_modules', 'src'],
   roots: ['<rootDir>/src'],
   transform: {
     '^.+\\.[t|j]sx?$': 'ts-jest',
