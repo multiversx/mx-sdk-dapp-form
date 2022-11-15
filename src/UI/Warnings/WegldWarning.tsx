c18936c1efb19e0b1ac63f8130f53cf54fb80c99
Tudor Morar <tudor.morar@elrond.com>
2022-11-15 12:01:01 +0300

    Merge with development

--- src/UI/Warnings/WegldWarning.tsx
+++ src/UI/Warnings/WegldWarning.tsx
@@ -3,39 +3,29 @@ import { faExclamationTriangle } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { WEGLD_ID, WEGLD_MESSAGE } from 'constants/index';
 
+import styles from './styles.module.scss';
+
 export const WegldWarning = ({ tokenId }: { tokenId: string }) => {
   if (WEGLD_ID !== tokenId) {
     return null;
   }
 
   return (
-    <div className='row'>
-      <div className='col-12'>
-        <small
-          className='wegld-alert-warning align-items-center d-flex p-3'
-          role='alert'
-        >
-          <FontAwesomeIcon
-            icon={faExclamationTriangle}
-            size='lg'
-            className='wegld-alert-icon mr-2'
-          />
+    <div className={styles.canTransferWarning}>
+      <small role='alert' className={styles.wegldAlertWarning}>
+        <FontAwesomeIcon
+          icon={faExclamationTriangle}
+          className={styles.wegldAlertIcon}
+          size='lg'
+        />
 
-          <div>
-            <div className='font-weight-bold'>Warning</div>
-            <div>{WEGLD_MESSAGE} </div>
-            <a
-              {...{
-                target: '_blank'
-              }}
-              className='d-none'
-              href='https://elrond.com/'
-            >
-              Learn more
-            </a>
+        <div className={styles.canTransferWarningWrapper}>
+          <div className={styles.canTransferWarningLabel}>Warning</div>
+          <div className={styles.canTransferWarningMessage}>
+            {WEGLD_MESSAGE}
           </div>
-        </small>
-      </div>
+        </div>
+      </small>
     </div>
   );
 };
