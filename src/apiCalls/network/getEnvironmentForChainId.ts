25c193d8b47777aaecdcbcbbbd93bbdd2dc663cc
Tudor Morar <tudor.morar@elrond.com>
2022-12-21 14:46:07 +0300

    Fix WEGLD warning

--- src/apiCalls/network/getEnvironmentForChainId.ts
+++ src/apiCalls/network/getEnvironmentForChainId.ts
@@ -1,9 +1,21 @@
+import { WEGLD_ID } from 'constants/index';
+
 const chainIDToEnvironment: Record<string, string> = {
   D: 'devnet',
   T: 'testnet',
   '1': 'mainnet'
 };
 
+const chainIDToWegldId: Record<string, string> = {
+  D: 'WEGLD-d7c6bb',
+  T: 'WEGLD-7fbb90',
+  '1': 'WEGLD-bd4d79'
+};
+
 export function getEnvironmentForChainId(chainId: string) {
   return chainIDToEnvironment[chainId];
+}
+
+export function getWegldIdForChainId(chainId: string) {
+  return chainIDToWegldId[chainId] || WEGLD_ID;
 }
