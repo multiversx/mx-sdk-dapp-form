b25a7634713bc5a0cd8fd3c7a10edcca0e566b5f
Miro Mărgineanu <miro.margineanu@elrond.com>
2023-01-02 22:53:32 +0200

    [*] Fix amount slider display condition.

--- src/UI/Form/Form.tsx
+++ src/UI/Form/Form.tsx
@@ -73,7 +73,7 @@ export const Form = ({ className }: WithClassnameType) => {
 
         <Amount />
 
-        {!uiOptions?.showAmountSlider && !isNFTTransaction && (
+        {uiOptions?.showAmountSlider && !isNFTTransaction && (
           <AmountSlider
             onPercentageChange={onSetAmountPercentage}
             percentageValue={amountRange}
