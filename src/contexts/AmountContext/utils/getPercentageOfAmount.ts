d77f3c8c5bc999ee79b919651f3f89f984682a73
Miro Mărgineanu <miro.margineanu@elrond.com>
2023-01-03 15:23:04 +0200

    [*] Variable rename

--- src/contexts/AmountContext/utils/getPercentageOfAmount.ts
+++ src/contexts/AmountContext/utils/getPercentageOfAmount.ts
@@ -8,12 +8,12 @@ export const getPercentageOfAmount = (
   maxAmountMinusDust: string
 ) => {
   const value = stringIsFloat(String(amount)) ? amount : ZERO;
-  const maxAmountMinusDustBN = stringIsFloat(String(maxAmountMinusDust))
+  const maxBalanceBn = stringIsFloat(String(maxAmountMinusDust))
     ? new BigNumber(maxAmountMinusDust)
     : new BigNumber(ZERO);
 
   const percentage = BigNumber(100)
-    .dividedBy(maxAmountMinusDustBN.dividedBy(value))
+    .dividedBy(maxBalanceBn.dividedBy(value))
     .toNumber();
 
   return BigNumber.minimum(percentage, 100).toNumber();
