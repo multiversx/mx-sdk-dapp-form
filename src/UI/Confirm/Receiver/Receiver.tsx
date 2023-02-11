37f460d34a80bd1f190d4299c7c6283a1adafd90
Radu Mojic <radu.mojic@elrond.com>
2023-02-11 17:47:18 +0200

    [+] use the same context labels on the confirm page as on the send page for consistency

--- src/UI/Confirm/Receiver/Receiver.tsx
+++ src/UI/Confirm/Receiver/Receiver.tsx
@@ -3,6 +3,7 @@ import { faExclamationTriangle } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 import globals from 'assets/sass/globals.module.scss';
+import { useUICustomizationContext } from 'contexts/UICustomization';
 import styles from './styles.module.scss';
 
 export interface ReceiverPropsType {
@@ -15,21 +16,29 @@ export const Receiver = ({
   label = 'Receiver',
   receiver,
   scamReport
-}: ReceiverPropsType) => (
-  <div className={styles.receiver}>
-    <span className={globals.label}>{label}</span>
-    {receiver && <span className={globals.value}>{receiver}</span>}
+}: ReceiverPropsType) => {
+  const {
+    fields: {
+      receiver: { label: contextLabel }
+    }
+  } = useUICustomizationContext();
 
-    {scamReport && (
-      <div className={styles.scam}>
-        <span>
-          <FontAwesomeIcon
-            icon={faExclamationTriangle}
-            className={styles.icon}
-          />
-          <small>{scamReport}</small>
-        </span>
-      </div>
-    )}
-  </div>
-);
+  return (
+    <div className={styles.receiver}>
+      <span className={globals.label}>{contextLabel || label}</span>
+      {receiver && <span className={globals.value}>{receiver}</span>}
+
+      {scamReport && (
+        <div className={styles.scam}>
+          <span>
+            <FontAwesomeIcon
+              icon={faExclamationTriangle}
+              className={styles.icon}
+            />
+            <small>{scamReport}</small>
+          </span>
+        </div>
+      )}
+    </div>
+  );
+};
