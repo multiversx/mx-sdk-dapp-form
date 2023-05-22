3a1937ae8c142e41e458260905604d1ad9b880dc
Tudor Morar <tudor.morar@multiversx.com>
2023-05-22 16:44:28 +0300

    Extend tests for guardian (#175)
    * Extend tests for guardian

---
+++ src/tests/SendWithGuardian.test.tsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import { GuardianScreenType } from '@multiversx/sdk-dapp/types/transactions.types';
+import { fireEvent } from '@testing-library/react';
+import { testAddress } from '__mocks__';
+import { renderForm } from 'tests/helpers/renderForm';
+import { formConfiguration, sendAndConfirmTest } from './helpers';
+
+const GuardianScreen = (props: GuardianScreenType) => {
+  return (
+    <div data-testid='guardianScreen'>
+      <button onClick={props.onPrev} data-testid='guardianBackButton'>
+        Back
+      </button>
+    </div>
+  );
+};
+
+describe('Guardian screen tests', () => {
+  test('Guardian screen visibility and navigation', async () => {
+    const methods = renderForm({
+      balance: '1_000_000_000_000_000'.replaceAll('_', ''), // 0.001,
+      isGuarded: true,
+      GuardianScreen,
+      formConfigValues: {
+        ...formConfiguration
+      }
+    });
+
+    const receiver: any = await methods.findByTestId('receiver');
+
+    fireEvent.change(receiver, { target: { value: testAddress } });
+
+    const amount: any = await methods.findByTestId('amount');
+    fireEvent.change(amount, { target: { value: '0.00001' } });
+    fireEvent.blur(amount, { target: { value: '0.00001' } });
+
+    const sendButton = methods.getByTestId('sendBtn');
+    fireEvent.click(sendButton);
+
+    await sendAndConfirmTest({ methods })({
+      amount: '0.00001',
+      fee: '0.0000505 xEGLD'
+    });
+
+    // account is guarded and has GuardianComponent
+    // after pressing send, GuardianScreen should be visible
+    const sendTransactionBtn = methods.getByTestId('sendTrxBtn');
+    fireEvent.click(sendTransactionBtn);
+    const guardianScreen = await methods.findByTestId('guardianScreen');
+    expect(guardianScreen).toBeDefined();
+
+    // on Guardian screen back, Confirm Screen should be visible
+    const guardianBackButton = methods.getByTestId('guardianBackButton');
+    fireEvent.click(guardianBackButton);
+    const confirmScreen = await methods.findByTestId('confirmScreen');
+    expect(confirmScreen).toBeDefined();
+  });
+});
