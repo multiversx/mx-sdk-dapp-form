74393f4a43540a198a5880284442468e2fa9a4d7
Tudor Morar <tudor.morar@multiversx.com>
2023-05-29 17:32:57 +0300

    Bring back EGLD amount tests

---
+++ src/tests/SendEGLD/SendEGLDnegative.test.ts
@@ -0,0 +1,22 @@
+import { fireEvent, waitFor } from '@testing-library/react';
+import { renderForm } from 'tests/helpers/renderForm';
+
+describe('EGLD Amount field', () => {
+  it.only('should be =< than balance - transaction fee', async () => {
+    const { queryByText, findByTestId } = renderForm();
+    const input: any = await findByTestId('amount');
+    const value = '-1';
+
+    const fullBalance = { target: { value } };
+    fireEvent.change(input, fullBalance);
+    fireEvent.blur(input);
+    expect(input.value).toBe(value);
+
+    await waitFor(() => {
+      expect(input.value).toBe(value);
+
+      const req = queryByText('Invalid number');
+      expect(req?.innerHTML).toBe('Invalid number');
+    });
+  });
+});
